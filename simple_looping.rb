3a91d0992ad62ce1ced2e5397d28f10e1b1608d3
Danny Wakeling <wakeling101@hotmail.com>
2019-03-01 15:32:06 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,7 @@
 phrase = "Welcome to Flatiron School's Web Development Course!"
 
 def loop_iterator(number_of_times)
+  phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   loop do
     puts phrase
