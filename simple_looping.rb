8803a411c4ee4e8b886e4f55f785b89b7280e146
Danny Wakeling <wakeling101@hotmail.com>
2019-03-01 15:30:04 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -17,7 +17,7 @@ end
 
 def while_iterator(number_of_times)
   counter = 0
-  while counter <= number_of_times
+  while counter <= number_of_times do
     puts phrase
     counter += 1
   end
@@ -25,9 +25,10 @@ end
 
 def until_iterator(number_of_times)
   counter = 0
-  until counter == number_of_times
+  until counter == number_of_times do
     puts phrase
     counter += 1
+  end
 end
 
 def for_iterator(number_of_times)
